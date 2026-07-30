b2b662114f8d7c6c8a5cf0371936ddef3ab16e71
Marly-z <babettesapouma@yahoo.fr>
2026-07-30 15:35:22 -0400

    Fix typo in constructor parameter name, oublie de e

--- src/Revue.java
+++ src/Revue.java
@@ -1,6 +1,6 @@
 public class Revue extends Document{
     //constructeur
-    public Revue(int id,String titre,String auteur,String type,int page,int anne){
+    public Revue(int id,String titre,String auteur,String type,int page,int annee){
         super(id,titre,auteur,type,page,anne);
     }
     @Override
