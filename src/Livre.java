dec3461c77212f47fbdf9834d365c2119369df71
Marly-z <babettesapouma@yahoo.fr>
2026-07-30 15:17:53 -0400

    Remove Empruntable interface from Livre class; Livre hérite de ce qui est dans document et document implémente déjà l'interface donc Livre hérite de l'implémentation

--- src/Livre.java
+++ src/Livre.java
@@ -1,4 +1,4 @@
-public class Livre extends Document implements Empruntable{
+public class Livre extends Document {
     //constructeur
     public Livre(int id,String titre,String auteur,String type,int page,int anne){
         super(id,titre,auteur,type,page,anne);
